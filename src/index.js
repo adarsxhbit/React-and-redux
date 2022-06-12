aa52cef4bccf82967bed3871f8911c1a9e2b939e
Adarsh Kesarwani <adarsh.kumar@bitclass.live>
2022-06-12 07:01:27 +0530

    S3- Props

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
+import faker from 'faker';
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CommentDetail  from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import './style/App.css'
 
 const App = () => {
-    const buttonText = {text: 'click me'}
-    const labelText = 'Enter Name:'
     return (
-        <div>
-            <label className = "label" htmlFor="name">
-                {labelText}
-            </label>
-            <input id="name" type = "text" />
-            <button style={{backgroundColor: "blue", color:"white"}}>
-                {buttonText.text}            
-            </button>
+        <div className='ui container comments'>
+            <ApprovalCard>
+                <CommentDetail author = "Adarsh" time="05:45AM"  comment="Nice Website" photo={faker.image.image()}/>            
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author = "Sagar" time="06:45AM"  comment="Helping" photo={faker.image.image()}/> 
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author = "Raj" time="07:45AM"  comment="Loved it" photo={faker.image.image()}/> 
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author = "Nirmal" time="08:45AM"  comment="Nice Blog" photo={faker.image.image()}/>    
+            </ApprovalCard>
         </div>
     );
 }
