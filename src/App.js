21fa37a8fa3258643572b5c91328aaca5a2003e1
Adarsh Kesarwani <adarsh.kumar@bitclass.live>
2022-07-12 23:41:03 +0530

    widgets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Accordion from "./component/Accordion";
+import Search from "./component/Search";
+import Dropdown from "./component/Dropdown";
+import Translate from "./component/Translate";
+import Route from "./component/Route";
+import Header from "./component/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const items = [
+    {
+        title: 'adarsh',
+        content: 'IIT KGP'
+    }, 
+    {
+        title: 'sagar',
+        content: 'Bangalore'
+    },
+    {
+        title: 'raj',
+        content: 'VIT bhopal'
+    }
+]
+
+const options = [
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The color blue',
+        value: 'blue'
+    },
+    {
+        label: 'The color red',
+        value: 'red'
+    }
+]
+
+export default () =>{
+    const [selected, setSelected]  = useState(options[0])
+
+    return (<div>
+        <Header/>
+        <Route path="/">
+            <Accordion items={items}/>
+        </Route>
+        <Route path="/list">
+            <Search/>
+        </Route>
+        <Route path="/translate">
+            <Translate/>
+        </Route>
+        <Route path="/dropdown">
+            <Dropdown label="select a language" options={options} selected={selected} onSelectedChange={setSelected}/>
+        </Route>
+    </div>)
+}   
